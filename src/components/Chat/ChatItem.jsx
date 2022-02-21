8027a73b398085d9abf695ab331822fefb9eb81a
IvanKashuba <kashubaivan7@gmail.com>
2022-02-21 03:13:09 +0200

    small fixes with scroll and messageArea

--- src/components/Chat/ChatItem.jsx
+++ src/components/Chat/ChatItem.jsx
@@ -3,10 +3,13 @@ import React from "react";
 
 const ChatItem = ({ text, time }) => {
   return (
-    <div className={styles.chatItem}>
-      <div className={styles.itemText}>{text}</div>
-      <div className={styles.time}>{time}</div>
-    </div>
+    <>
+      <div className={styles.chatItem}>
+        <div className={styles.itemText}>{text}</div>
+        <div className={styles.time}>{time}</div>
+      </div>
+      <div></div>
+    </>
   );
 };
 
